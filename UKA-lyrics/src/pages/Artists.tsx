c03c1112ec53da577e97ef355a9c208fc6ae455d
Johan Knudsen <johan.knudsen@ntnu.no>
2023-09-13 15:12:14 +0200

    Update Artists.tsx

--- UKA-lyrics/src/pages/Artists.tsx
+++ UKA-lyrics/src/pages/Artists.tsx
@@ -1,13 +1,6 @@
 function Artists () {
   return (
     <div className="w-15 h-100 bg-pink">
-       <button
-          onClick={() => {
-            window.location.href = "/opprett-gruppe";
-          }}
-          className="bg-green hover:bg-darksalmon duration-200 mb-4 text-white text-md text-left rounded-2xl w-full p-1 py-5 inline-flex items-center drop-shadow-box"
-        >
-          </button>
     </div>
   )
 }

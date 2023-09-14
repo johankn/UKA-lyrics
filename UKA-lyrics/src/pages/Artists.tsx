b7bf2034e0e06d6974c7253118a6551c749888e0
Rebekka Stangvik <rebekast@stud.ntnu.no>
2023-09-14 12:43:34 +0200

    resolve merge confilct

--- UKA-lyrics/src/pages/Artists.tsx
+++ UKA-lyrics/src/pages/Artists.tsx
@@ -81,68 +81,6 @@ function Artists() {
     </>
   );
 }
-//   return (
-//     <div className="pb-32 bg-background">
 
-//       <div className="max-w-md mx-auto pt-12 px-10 pb-4 bg-background h-full w-full">
-//       <button
-//         onClick={() => {
-//           window.location.href= "/HomePage.tsx";
-//         }}
-//         >
-//         <img src="./tilbakeKnapp.png" className="h-12 pb-1"></img>
-//       </button>
-//         <button
-//           onClick={() => {
-//             window.location.href = "/50CentPage";
-//           }}
-//           className="bg-pink hover:bg-darksalmon duration-200 mb-8 mt-6 text-darkblue text-md text-left rounded-2xl w-full p-1 py-5 inline-flex items-center drop-shadow-box">
-//           <span className="mx-auto text-xl">50 Cent</span>
-//         </button>
-
-//         <button
-//           onClick={() => {
-//             window.location.href = "/cezinandoPage";
-//           }}
-//           className="bg-green hover:bg-darksalmon duration-200 mb-8 text-darkblue text-md text-left rounded-2xl w-full p-1 py-5 inline-flex items-center drop-shadow-box">
-//           <span className="mx-auto text-xl">Cezinando</span>
-//         </button>
-
-//         <button
-//           onClick={() => {
-//             window.location.href = "/arifPage";
-//           }}
-//           className="bg-pink hover:bg-darksalmon duration-200 mb-8 text-darkblue text-md text-left rounded-2xl w-full p-1 py-5 inline-flex items-center drop-shadow-box">
-//           <span className="mx-auto text-xl">Arif</span>
-//         </button>
-
-//         <button
-//           onClick={() => {
-//             window.location.href = "/astridSPage";
-//           }}
-//           className="bg-green hover:bg-darksalmon duration-200 mb-8 text-darkblue text-md text-center rounded-2xl w-full p-1 py-5 inline-flex items-center drop-shadow-box">
-//           <span className="mx-auto text-xl">Astrid S</span>
-//         </button>
-
-//         <button
-//           onClick={() => {
-//             window.location.href = "/rubenPage";
-//           }}
-//           className="bg-pink hover:bg-darksalmon duration-200 mb-8 text-darkblue text-md text-center rounded-2xl w-full p-1 py-5 inline-flex items-center drop-shadow-box">
-//           <span className="mx-auto text-xl">Ruben</span>
-//         </button>
-
-//         <button
-//           onClick={() => {
-//             window.location.href = "/ramonPage";
-//           }}
-//           className="bg-green hover:bg-darksalmon duration-200 mb-4 text-darkblue text-md text-center rounded-2xl w-full p-1 py-5 inline-flex items-center drop-shadow-box">
-//           <span className="text-xl mx-auto">Ramon</span>
-//         </button>
-
-//       </div>
-//     </div>
-//   );
-// }
 
 export default Artists;
